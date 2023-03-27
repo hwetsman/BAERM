23f2723d3cf88663dd3b89b93926f9e7b55ea056
hwetsman <howard@tocdr.com>
2023-03-27 09:12:19 -0500

    Code: add today's model price to sidebar

--- nick.py
+++ nick.py
@@ -121,7 +121,7 @@ date_20240101 = proj_date.strftime('%m-%d-%Y')
 plot_df = df[(df['date'] > date_20190101) & (df['date'] < date_20240101)]
 
 # calculate today's baerm and display
-short_list = plot_df[plot_df['date'] <= dt.datetime.now()].PriceUSD.tolist()[-2:]
+short_list = plot_df[plot_df['date'] <= dt.datetime.now()].eYHAT.tolist()[-2:]
 if short_list[-1] == pd.notnull:
     today_baerm = short_list[-1]
 else:
