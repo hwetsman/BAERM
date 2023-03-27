dd51dae847437d5042c7a3595dea907fb8328ef6
hwetsman <howard@tocdr.com>
2023-03-27 09:51:58 -0500

    Code: add today's model price to sidebar

--- nick.py
+++ nick.py
@@ -128,6 +128,7 @@ today_baerm = plot_df[plot_df['date'] <= dt.datetime.now()].eYHAT.tolist()[-1]
 # else:
 #     today_baerm = short_list[-2]
 st.sidebar.write(f"Today's model value is ${round(today_baerm,2)}")
+st.sidebar.write('amended')
 
 # Calculate residuals
 # residuals = plot_df['logprice'] - plot_df['YHAT']
